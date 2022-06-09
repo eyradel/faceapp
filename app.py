dda57c6938cf203fb39aed28031ad193cf7b265e
eyradel <60956082+eyradel@users.noreply.github.com>
2022-06-09 11:35:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import cv2 as cv
+import cv2 
 hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
@@ -12,23 +12,23 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 from cvzone.FaceDetectionModule import FaceDetector
 detector = FaceDetector()
-video = cv.VideoCapture(0)
+video = cv2.VideoCapture(0)
 run = st.button("trigger")
 frame = st.image([])
 while run:
 	success,capture = video.read()
 	
 	img,bboxs = detector.findFaces(capture)
-	capture = cv.cvtColor(capture,cv.COLOR_BGR2RGB)
+	capture = cv2.cvtColor(capture,cv2.COLOR_BGR2RGB)
 
-	Blur = cv.GaussianBlur(capture,(5,5),2)
+	Blur = cv2.GaussianBlur(capture,(5,5),2)
 	
 	#frame.image(capture)
 	frame.image(Blur)
 stop = st.button("stop")
 
 if stop:
-	cv.destroyAllWindows()
+	cv2.destroyAllWindows()
 	video.release()
 
 
